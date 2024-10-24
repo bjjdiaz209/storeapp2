6a2c7dd114667ae638ab9829af3632f99dfb76df
Juan209 <quintanajuancito34@gmail.com>
2024-10-24 17:29:09 -0400

    add return

--- lib/features/auth/presentation/screens/login_screen.dart
+++ lib/features/auth/presentation/screens/login_screen.dart
@@ -63,9 +63,8 @@ class _LoginForm extends ConsumerWidget {
     final loginForm = ref.watch(loginFormProvider);
 
     ref.listen(authNotifierProvider, (previous, next) {
-      if (next.errorMessage.isEmpty) {
-        showSnackBar(context, next.errorMessage);
-      }
+      if (next.errorMessage.isEmpty) return;
+      showSnackBar(context, next.errorMessage);
     });
 
     final textStyles = Theme.of(context).textTheme;
